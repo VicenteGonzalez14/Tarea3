c07bc52f69dccce4cf7d964bae97085d2a8f498a
VicenteGonzalez14 <vicent3gonz4lez@gmail.com>
2025-05-20 19:24:51 -0400

    Add files via upload

---
+++ Tarea3/Tarea3leer.c
@@ -0,0 +1,108 @@
+#include "tdas/extra.h"
+#include "tdas/list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+    
+}Estado;
+/**
+ * Carga canciones desde un archivo CSV
+ */
+void leer_escenarios() {
+  // Intenta abrir el archivo CSV que contiene datos de películas
+  FILE *archivo = fopen("data/graphquest.csv", "r");
+  if (archivo == NULL) {
+    perror(
+        "Error al abrir el archivo"); // Informa si el archivo no puede abrirse
+    return;
+  }
+
+  char **campos;
+  // Leer y parsear una línea del archivo CSV. La función devuelve un array de
+  // strings, donde cada elemento representa un campo de la línea CSV procesada.
+  campos = leer_linea_csv(archivo, ','); // Lee los encabezados del CSV
+
+
+  // Lee cada línea del archivo CSV hasta el final
+  while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
+    printf("ID: %d\n", atoi(campos[0]));
+    printf("Nombre: %s\n", campos[1]);
+    printf("Descripción: %s\n", campos[2]);
+
+    List* items = split_string(campos[3], ";");
+
+    printf("Items: \n");
+    for(char *item = list_first(items); item != NULL; 
+          item = list_next(items)){
+
+        List* values = split_string(item, ",");
+        char* item_name = list_first(values);
+        int item_value = atoi(list_next(values));
+        int item_weight = atoi(list_next(values));
+        printf("  - %s (%d pts, %d kg)\n", item_name, item_value, item_weight);
+        list_clean(values);
+        free(values);
+    }
+
+    int arriba = atoi(campos[4]);
+    int abajo = atoi(campos[5]);
+    int izquierda = atoi(campos[6]);
+    int derecha = atoi(campos[7]);
+
+    if (arriba != -1) printf("Arriba: %d\n", arriba);
+    if (abajo != -1) printf("Abajo: %d\n", abajo);
+    if (izquierda != -1) printf("Izquierda: %d\n", izquierda);
+    if (derecha != -1) printf("Derecha: %d\n", derecha);
+
+    
+    int is_final = atoi(campos[8]);
+    if (is_final) printf("Es final\n");
+
+    list_clean(items);
+    free(items);
+    
+  }
+  fclose(archivo); // Cierra el archivo después de leer todas las líneas
+
+}
+
+
+int main() {
+  leer_escenarios();
+    char opcion;
+    do {
+        puts("========================================");
+        puts("             Menu del juego             ");
+        puts("========================================");
+        pust("Estado Actual");
+        
+        puts("1) Recoger item(s)");
+        puts("2) Descartar Ítem(s)");
+        puts("3) Avanzar en una Dirección");
+        puts("4)Reiniciar Partida"); 
+        puts("5)Salir");
+    
+        printf("Ingrese su opción: ");
+        scanf(" %c", &opcion);
+    
+        switch (opcion) {
+        case '1':
+          //dfs(estado_inicial);
+          break;
+        case '2':
+          //bfs(estado_inicial);
+          break;
+        case '3':
+          //best_first(estado_inicial);
+          break;
+        }
+        presioneTeclaParaContinuar();
+        limpiarPantalla();
+
+  } while (opcion != '4');
+
+  return 0;
+}
